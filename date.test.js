3d4cb0c66505c8b941008021a21e6e8a199ea9d9
BahlaievSerhii <111654343+BahlaievSerhii@users.noreply.github.com>
2023-02-19 22:49:12 +0200

    Add files via upload

---
+++ date.test.js
@@ -0,0 +1,9 @@
+import { exportAllDeclaration } from '@babel/types'
+import { functions } from './date.js'
+
+describe("date.test", () => {
+    test ("",() => {
+        const now = function compareDate();
+        expect (now).toBe(false);
+    })
+})
