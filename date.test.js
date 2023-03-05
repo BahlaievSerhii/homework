838546ae3678e8180171a6994b28e3726e35b37e
BahlaievSerhii <111654343+BahlaievSerhii@users.noreply.github.com>
2023-03-05 22:50:07 +0200

    Delete date.test.js

--- date.test.js
+++
@@ -1,9 +0,0 @@
-import { exportAllDeclaration } from '@babel/types'
-import { functions } from './date.js'
-
-describe("date.test", () => {
-    test ("",() => {
-        const now = function compareDate();
-        expect (now).toBe(false);
-    })
-})
